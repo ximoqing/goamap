bf1a2eab75d67a1284e963eacc79c94591ed547f
ximoqing <ximoqing@miyear.club>
2022-03-07 17:30:20 +0800

    修改

--- web.go
+++ web.go
@@ -6,11 +6,11 @@ import (
 	"net/url"
 )
 
-var Amapapi = "https://restapi.amap.com/"
+var amapapi = "https://restapi.amap.com/"
 
 // Ip ip定位
 func IpJson(key, ip string) string {
-	url := Amapapi + "v3/ip?ip=" + ip + "&output=json&key=" + key
+	url := amapapi + "v3/ip?ip=" + ip + "&output=json&key=" + key
 	req, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -22,7 +22,7 @@ func IpJson(key, ip string) string {
 }
 
 func IpXml(key, ip string) string {
-	url := Amapapi + "v3/ip?ip=" + ip + "&output=xml&key=" + key
+	url := amapapi + "v3/ip?ip=" + ip + "&output=xml&key=" + key
 	req, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -35,7 +35,7 @@ func IpXml(key, ip string) string {
 
 // Ipv2 ip定位v2
 func Ipv2(key, typ, ip string) string {
-	ur := Amapapi + "v5/ip?parameters"
+	ur := amapapi + "v5/ip?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
@@ -58,7 +58,7 @@ func Ipv2(key, typ, ip string) string {
 
 // 地理/逆地理编码
 func Georegeo(key, address string) string {
-	ur := Amapapi + "v3/geocode/geo?parameters"
+	ur := amapapi + "v3/geocode/geo?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
@@ -80,7 +80,7 @@ func Georegeo(key, address string) string {
 
 // 路径规划
 func Direction(key, origin, destination string) string {
-	ur := Amapapi + "v3/direction/walking?parameters"
+	ur := amapapi + "v3/direction/walking?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
@@ -103,7 +103,7 @@ func Direction(key, origin, destination string) string {
 
 // 路径规划v2
 func DirectionV2(key, origin, destination string) string {
-	ur := Amapapi + "v5/direction/driving?parameters"
+	ur := amapapi + "v5/direction/driving?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
@@ -126,7 +126,7 @@ func DirectionV2(key, origin, destination string) string {
 
 // 行政区域查询
 func District(key, keywords string) string {
-	ur := Amapapi + "v3/config/district?parameters"
+	ur := amapapi + "v3/config/district?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
@@ -148,7 +148,7 @@ func District(key, keywords string) string {
 
 // 天气查询
 func Weatherinfo(key, city string) string {
-	ur := Amapapi + "v3/weather/weatherInfo?parameters"
+	ur := amapapi + "v3/weather/weatherInfo?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
@@ -170,7 +170,7 @@ func Weatherinfo(key, city string) string {
 
 // 输入提示
 func Inputtips(key, keywords string) string {
-	ur := Amapapi + "v3/assistant/inputtips?parameters"
+	ur := amapapi + "v3/assistant/inputtips?parameters"
 	req, err := http.NewRequest(http.MethodGet, ur, nil)
 	if err != nil {
 		panic(err)
